2dd69e7cce6e403dd35a5900bdf0f3023be58e17
Fares Masmoudi <faresmasmoudi@outlook.com>
2024-05-05 14:22:45 +0100

    update

--- shop_db.sql
+++ shop_db.sql
@@ -79,17 +79,17 @@ CREATE TABLE `messages` (
 --
 
 CREATE TABLE `orders` (
-  `id` int(100) NOT NULL,
-  `user_id` int(100) NOT NULL,
-  `name` varchar(20) NOT NULL,
-  `number` varchar(10) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `method` varchar(50) NOT NULL,
-  `address` varchar(500) NOT NULL,
-  `total_products` varchar(1000) NOT NULL,
-  `total_price` int(100) NOT NULL,
-  `placed_on` date NOT NULL DEFAULT current_timestamp(),
-  `payment_status` varchar(20) NOT NULL DEFAULT 'pending'
+                          `id` int(100) NOT NULL,
+                          `user_id` int(100) NOT NULL,
+                          `name` varchar(20) NOT NULL,
+                          `number` varchar(10) NOT NULL,
+                          `email` varchar(50) NOT NULL,
+                          `method` varchar(50) NOT NULL,
+                          `address` varchar(500) NOT NULL,
+                          `total_products` varchar(1000) NOT NULL,
+                          `total_price` int(100) NOT NULL,
+                          `placed_on` datetime NOT NULL DEFAULT current_timestamp(),
+                          `payment_status` varchar(20) NOT NULL DEFAULT 'pending'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
